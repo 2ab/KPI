5f7538960da8c8d5cdfcfa9412b697a7e14b698e
Gampol Thitinilnithi <gampolt@gmail.com>
2013-01-09 14:18:14 +0700

    Timer badge and experiment on typeahead text input

--- client/client.js
+++ client/client.js
@@ -1,24 +1,26 @@
 /////////////////////////////////////
 // Status Template
 ////////////////////////////////////
-Template.status.status = function () {
-  return Meteor.status().status;
-}
 
-Template.status.msg = function () {
-  st = Meteor.status();
-  retry_in = ((st.retryTime-(new Date()).getTime())/1000).toFixed(2);
-  return "Retry #" + st.retry_count + 
-    " ,retry again in "+ retry_in + " s."
-}
+Meteor.startup( function () {
+  Session.set('now', utils.now());
+  setInterval(function () {
+    Session.set('now', utils.now());
+  }, 20 * 1000);
+});
 
 Template.status.connected = function () {
   return Meteor.status().connected;
 }
 
 Template.status.datetime = function () {
-  var d = new Date()
-  return d.toLocaleDateString() + " " + d.toLocaleTimeString();
+  return Session.get('now');
 }
 
-Meteor.setInterval(Template.status.datetime, 1000);
+Template.myform.rendered = function () {
+  return Meteor.defer(function () {
+    return $('.item-select').typeahead({
+      source: items
+    });
+  });
+};
---
+++ shared.js
@@ -0,0 +1,4 @@
+Meteor.startup(function () {
+  items = ['somsak','tossaporn','gampol','taweewat','kamonrat','panuwat',
+    'wuthisak','nikom','kriengsak','somsri','nikorn','arunee','stephen boyd']
+});
